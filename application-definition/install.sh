c246f96f2496a7249c8468dec94d98bb8e2be606
Daan Willems <daan@daan.nl>
2024-03-31 15:32:37 +0000

    Add nginx example config for misp to installation script

--- application-definition/install.sh
+++ application-definition/install.sh
@@ -5,6 +5,7 @@ cp -r core /etc/containers/systemd/misp/
 cp -r modules /etc/containers/systemd/misp/
 cp -r redis /etc/containers/systemd/misp/
 cp -r nginx /etc/containers/systemd/misp/
+cp -f ./nginx/*.conf /media/misp/nginx-config/
 
 mkdir -p /media/misp/db
 mkdir -p /media/misp/config
